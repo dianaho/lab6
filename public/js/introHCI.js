71428ae11582d4a25bc8e7c6c61d29f27fde07af
Diana Ho <dianahox3@gmail.com>
2016-02-11 10:32:09 +0000

    AJAX lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,8 +26,26 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	//Call AJAX endpoint
+	$.get("/project/" + idNumber, projectClick);
+
 	console.log("User clicked on project " + idNumber);
 }
+
+// Callback function
+function projectClick(result){
+	console.log(result);
+
+	var projectHTML = '<a href="#" class="thumbnail">' + 
+	'<img src="' + result['image'] + '"class="img">' +
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p></a>' +
+	'<p>' + result['summary'] + '</p>';
+
+	$("#project" + result['id'] + " .details").html(projectHTML);
+}
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +53,19 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", colorPage);
+}
+
+function colorPage(result) {
+	console.log(result);
+
+	var color = result['colors'];
+	var colors = color.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
